9b63320541d0a4ec23830d7367109a92cc8adc25
jenniferkwok <jenwkwok@gmail.com>
2018-11-29 01:06:05 -0800

    linked css file and react bootstrap

--- music-master/src/App.jsx
+++ music-master/src/App.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import './App.css'
+
 class App extends Component {
   render(){
     return(
-      <div>Music Master from App</div>
+      <div className="app">
+        <div className="app-title">Music Master</div>
+        <div>
+          <input placeholder="search an artist..." />
+          <button>search</button>
+        </div>
+        <div className="profile">
+          <div>artist picture</div>
+          <div>artist name</div>
+        </div>
+        <div className="gallery">
+        gallery
+        </div>
+      </div>
     )
   }
 }
