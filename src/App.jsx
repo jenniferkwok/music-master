29804ea6fd22413985b1d4d543a86824803a7fdd
jenniferkwok <jenwkwok@gmail.com>
2018-12-01 23:58:25 -0800

    search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { FormGroup, FormControl, InputGroup };
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
   constructor(props){
